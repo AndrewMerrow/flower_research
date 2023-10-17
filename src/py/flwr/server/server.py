1b7de315b5fcb0cab5cbabc55adabf3de172ebca
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-10-17 12:09:32 -0500

    Truncation test

--- src/py/flwr/server/server.py
+++ src/py/flwr/server/server.py
@@ -120,6 +120,8 @@ class Server:
             if res_fit is not None:
                 parameters_prime, fit_metrics, _ = res_fit  # fit_metrics_aggregated
                 if parameters_prime:
+                    print("Param truncate check")
+                    print(parameters_prime)
                     test_params = parameters_to_ndarrays(self.parameters) + parameters_to_ndarrays(parameters_prime)
                     print(np.array_equal(test_params, parameters_to_ndarrays(self.parameters)))
                     self.parameters = ndarrays_to_parameters(test_params)
