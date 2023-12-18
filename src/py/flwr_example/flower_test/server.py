e49393dd501b5b9edfbec9c91426bdd93981d17d
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-12-18 10:22:00 -0600

    Larger test

--- src/py/flwr_example/flower_test/server.py
+++ src/py/flwr_example/flower_test/server.py
@@ -301,11 +301,11 @@ def main():
     model_parameters = [val.cpu().numpy() for _, val in model.state_dict().items()]
 
     # Create strategy
-    num_agents = 1
+    num_agents = 10
     #strategy = fl.server.strategy.FedAvg(
     strategy = AggregateCustomMetricStrategy(
-        min_fit_clients=num_agents,
-        min_evaluate_clients=num_agents,
+        #min_fit_clients=num_agents,
+        #min_evaluate_clients=num_agents,
         fraction_fit=.01,
         fraction_evaluate=.01,
         min_available_clients=num_agents,
