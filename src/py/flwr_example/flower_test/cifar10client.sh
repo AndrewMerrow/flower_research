43cbaec8e4591541b0e4fafd5ee70bc8cacbfc1f
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-12-18 10:37:40 -0600

    10 client cifar script

---
+++ src/py/flwr_example/flower_test/cifar10client.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
+
+# Download the CIFAR-10 dataset
+#python -c "from torchvision.datasets import CIFAR10; CIFAR10('./dataset', download=True)"
+
+#Start the poisoned clients
+for i in `seq 0 9`; do
+    echo "Starting client $i"
+    python client.py --clientID $i --data "cifar10" &
+done
+
+# Enable CTRL+C to stop all background processes
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
+# Wait for all background processes to complete
+wait
