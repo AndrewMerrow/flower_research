168747c292a0e36bdc5af7b0a22620ba667435b0
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-01-04 10:59:12 -0600

    Return the parameters that the detection modified

--- src/py/flwr_example/flower_test/server.py
+++ src/py/flwr_example/flower_test/server.py
@@ -258,7 +258,8 @@ class AggregateCustomMetricStrategy(fl.server.strategy.FedAvgM):
         print(f"Round {server_round} poison accuracy aggregated from client fit results: {aggregated_poison_accuracy}")
 
         # Return aggregated model paramters and other metrics (i.e., aggregated accuracy)
-        return ndarrays_to_parameters(weights_prime), {"accuracy": aggregated_accuracy}
+        #return ndarrays_to_parameters(weights_prime), {"accuracy": aggregated_accuracy}
+        return model.parameters(), {"accuracy": aggregated_accuracy}
     
     def aggregate_evaluate(
         self,
