e4653d9cd6fe4288cedf77af3e6cae27137f043c
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-01-15 12:57:31 -0600

    Test bengin counter

--- src/py/flwr_example/flower_test/testAnalysis.py
+++ src/py/flwr_example/flower_test/testAnalysis.py
@@ -1,26 +1,32 @@
 from prettytable import PrettyTable
 from texttable import Texttable
 
-predicted_malicious = []
-with open("cifarOutput.txt", "r") as f:
-    lines = f.readlines()
-    for line in lines:
-        if("[" in line):
-            predicted_malicious.append(line.rstrip('\n'))
+def countMaliciousFlags(predicted_malicious):
+    #print(predicted_malicious)
+    #table = PrettyTable(['Client', 'Malicious Flags'])
+    table = Texttable()
+    table.add_rows([["Client", "Malicious Flags", "Benign Flags"]])
+    times_flagged = {}
+    for client in range(30):
+        mal_counter = 0
+        ben_counter = 0
+        for round in predicted_malicious:
+            if(" {},".format(client) in round):
+                #print(round)
+                mal_counter += 1
+            else:
+                ben_counter += 1
+        times_flagged[client] = (mal_counter, ben_counter)
+    #print(times_flagged)
+    for key, value in times_flagged.items():
+        table.add_row([key, value[0], value[1]])
+    print(table.draw())
 
-#print(predicted_malicious)
-#table = PrettyTable(['Client', 'Malicious Flags'])
-table = Texttable()
-table.add_rows([["Client", "Malicious Flags"]])
-times_flagged = {}
-for client in range(30):
-    counter = 0
-    for round in predicted_malicious:
-        if(" {},".format(client) in round):
-            #print(round)
-            counter += 1
-    times_flagged[client] = counter
-#print(times_flagged)
-for key, value in times_flagged.items():
-    table.add_row([key, value])
-print(table.draw())
+def main():
+    predicted_malicious = []
+    with open("cifarOutput.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if("[" in line):
+                predicted_malicious.append(line.rstrip('\n'))
+    countMaliciousFlags(predicted_malicious)
