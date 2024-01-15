9517c4b380569160009ef811980f69aa35625946
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-01-15 12:29:32 -0600

    Test

--- src/py/flwr_example/flower_test/testAnalysis.py
+++ src/py/flwr_example/flower_test/testAnalysis.py
@@ -1,5 +1,8 @@
+predicted_malicious = []
 with open("cifarOutput.txt", "r") as f:
     lines = f.readlines()
     for line in lines:
         if("[" in line):
-            print(line)
+            predicted_malicious.append(line)
+
+print(predicted_malicious)
