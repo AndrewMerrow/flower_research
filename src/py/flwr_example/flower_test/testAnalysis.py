599cc2afe5ce90e8863baea5ad6b562da1124198
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-01-15 12:31:11 -0600

    Print all 3s

--- src/py/flwr_example/flower_test/testAnalysis.py
+++ src/py/flwr_example/flower_test/testAnalysis.py
@@ -5,4 +5,7 @@ with open("cifarOutput.txt", "r") as f:
         if("[" in line):
             predicted_malicious.append(line.rstrip('\n'))
 
-print(predicted_malicious)
+#print(predicted_malicious)
+for round in predicted_malicious:
+    if(" 3," in round):
+        print(round)
