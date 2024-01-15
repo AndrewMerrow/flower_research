70677ba3ce7e45792191563591f3879e7c7acc58
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-01-15 12:32:02 -0600

    Count 3s

--- src/py/flwr_example/flower_test/testAnalysis.py
+++ src/py/flwr_example/flower_test/testAnalysis.py
@@ -6,6 +6,9 @@ with open("cifarOutput.txt", "r") as f:
             predicted_malicious.append(line.rstrip('\n'))
 
 #print(predicted_malicious)
+counter = 0
 for round in predicted_malicious:
     if(" 3," in round):
         print(round)
+        counter += 1
+print(counter)
