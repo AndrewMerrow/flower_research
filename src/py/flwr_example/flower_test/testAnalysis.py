948a9aa15dc17a22c1f1168fb1bf922f141ec0eb
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-01-17 12:08:21 -0600

    Adding fedemnist functionality

--- src/py/flwr_example/flower_test/testAnalysis.py
+++ src/py/flwr_example/flower_test/testAnalysis.py
@@ -17,24 +17,43 @@ def retrieveAccuracy(table, accuracies, poison_accuracies):
         table.add_row([i, '{:.2%}'.format(float(accuracy)), '{:.2%}'.format(float(poison_accuracy))])
     return(table)
 
-def countMaliciousFlags(table, predicted_malicious):
+def countMaliciousFlags(args, table, predicted_malicious, selected_clients = None):
     #print(predicted_malicious)
     #table = PrettyTable(['Client', 'Malicious Flags'])
     table.add_row(["Client", "Malicious Flags", "Benign Flags"])
     times_flagged = {}
-    for client in range(40):
-        mal_counter = 0
-        ben_counter = 0
-        for round in predicted_malicious:
-            if(" {},".format(client) in round or "[{}".format(client) in round or " {}]".format(client) in round):
-                #print(round)
-                mal_counter += 1
-            else:
-                ben_counter += 1
-        times_flagged[client] = (mal_counter, ben_counter)
-    #print(times_flagged)
-    for key, value in times_flagged.items():
-        table.add_row([key, value[0], value[1]])
+    num_clients = 0
+    if("cifar" in args.file):
+        num_clients = 40
+        for client in range(num_clients):
+            mal_counter = 0
+            ben_counter = 0
+            for round in predicted_malicious:
+                if(" {},".format(client) in round or "[{}".format(client) in round or " {}]".format(client) in round):
+                    #print(round)
+                    mal_counter += 1
+                else:
+                    ben_counter += 1
+            times_flagged[client] = (mal_counter, ben_counter)
+        #print(times_flagged)
+        for key, value in times_flagged.items():
+            table.add_row([key, value[0], value[1]])
+
+    elif("fedemnist" in args.file):
+        num_clients = 3383
+        for client in range(num_clients):
+            mal_counter = 0
+            ben_counter = 0
+            for round in predicted_malicious:
+                if((" {},".format(client) in round or "[{}".format(client) in round or " {}]".format(client) in round) and (" {},".format(client) in selected_clients or "[{}".format(client) in selected_clients or " {}]".format(client) in selected_clients)):
+                    mal_counter += 1
+                elif(" {},".format(client) in selected_clients or "[{}".format(client) in selected_clients or " {}]".format(client) in selected_clients):
+                    ben_counter += 1
+            times_flagged[client] = (mal_counter, ben_counter)
+            
+        for key, value in times_flagged.items():
+            table.add_row([key, value[0], value[1]])
+    
     #print(table.draw())
     return(table)
 
@@ -54,17 +73,22 @@ def main():
     predicted_malicious = []
     accuracies = []
     poison_accuracies = []
+    selected_clients = []
     with open(args.file, "r") as f:
         lines = f.readlines()
         for line in lines:
-            if("[" in line):
+            if("[" in line and "malicious" in line):
                 predicted_malicious.append(line.rstrip('\n'))
             elif("poison" not in line and "accuracy:" in line):
                 accuracies.append(line.rstrip('\n'))
             elif("poison" in line):
                 poison_accuracies.append(line.rstrip("\n"))
 
-    table = countMaliciousFlags(table, predicted_malicious)
+            if("fedemnist" in args.file):
+                if("selected clients" in line):
+                    selected_clients.append(line.rstrip('\n'))
+
+    table = countMaliciousFlags(args, table, predicted_malicious, selected_clients)
     print(table.draw())
     accuracyTable = retrieveAccuracy(accuracyTable, accuracies, poison_accuracies)
     print(accuracyTable.draw())
