7ab6dadec2722c033d3e4145284db70972be4759
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-02-22 14:55:34 -0600

    test

--- src/py/flwr_example/flower_test/server.py
+++ src/py/flwr_example/flower_test/server.py
@@ -326,12 +326,8 @@ class AggregateCustomMetricStrategy(fl.server.strategy.FedAvgM):
             X1, clients, malicious = our_detection_v3.extract_features_minmax(detection_slice, selectedDataset)
             lof_predicted_benign, lof_predicted_malicious = our_detection_v3.local_outlier_factor(X1, clients, 0.1)
             print ('lof prediction benign:', sorted(lof_predicted_benign))
-            print(detection_slice)
             
-            print(list(map(str, lof_predicted_benign)))
             filtered_dataset = detection_slice.filter(items=list(map(int, lof_predicted_benign)))
-            #filtered_dataset = detection_slice
-            print(filtered_dataset)
             X2, clients, malicious = our_detection_v3.extract_features_tsne(filtered_dataset, selectedDataset)
             kmeans_predicted_malicious = our_detection_v3.kmeans_clustering(X2, clients)
             print ('kmeans malicious prediction:', sorted(kmeans_predicted_malicious))
