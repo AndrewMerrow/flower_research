fcac2619c508e461a24bf980583f670012609f1c
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-03-08 14:00:51 -0600

    change to lof directory

--- src/py/flwr_example/flower_test/server.py
+++ src/py/flwr_example/flower_test/server.py
@@ -803,7 +803,7 @@ def main():
     else:
         model = utils.CNN_MNIST()
         ct = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        filename = "hybrid/offset0.1/lof_offset_01_poison_100_test_fedemnist_33_clients_" + str(ct) + ".txt"
+        filename = "lof/offset0.1/lof_offset_01_poison_100_test_fedemnist_33_clients_" + str(ct) + ".txt"
         with open(filename, "w") as f:
             print("Running fedemnist test", file=f)
 
