cf308c85c8c2540c1401fb8a050cca3409084d57
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-03-11 10:19:11 -0500

    hybrid 33 clients poison 20 test

--- src/py/flwr_example/flower_test/server.py
+++ src/py/flwr_example/flower_test/server.py
@@ -803,7 +803,7 @@ def main():
     else:
         model = utils.CNN_MNIST()
         ct = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        filename = "hybrid/offset0.1/hybrid_offset_01_poison_50_test_fedemnist_33_clients_" + str(ct) + ".txt"
+        filename = "hybrid/offset0.1/hybrid_offset_01_poison_20_test_fedemnist_33_clients_" + str(ct) + ".txt"
         with open(filename, "w") as f:
             print("Running fedemnist test", file=f)
 
