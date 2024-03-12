f86bb97284baa89c9f3f7142eaa98510b6bbd633
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-03-12 12:47:59 -0500

    test

--- src/py/flwr_example/flower_test/directoryAnalysis.py
+++ src/py/flwr_example/flower_test/directoryAnalysis.py
@@ -166,7 +166,7 @@ def main():
     roundGroupTable = Texttable()
 
     #the path of the directory containing the files we want to analyize 
-    p = Path('./directoryAnalysis/lof/all')
+    p = Path('./directoryAnalysis/test')
     for child in p.iterdir():
         if child.is_file():
             #save the path of the current file
