9bc5f5e018ea41e69536f1381ec0adb4bbe1a8cf
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-03-25 11:33:13 -0500

    Clients updated to use all GPUs

--- src/py/flwr_example/flower_test/client2.py
+++ src/py/flwr_example/flower_test/client2.py
@@ -267,7 +267,7 @@ def main() -> None:
     args = parser.parse_args()
 
     device = torch.device(
-        "cuda:0" if torch.cuda.is_available() and args.use_cuda else "cpu"
+        "cuda:1" if torch.cuda.is_available() and args.use_cuda else "cpu"
     )
 
     if args.dry:
