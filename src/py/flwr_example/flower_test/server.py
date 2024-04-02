c43f926e3970dab78540bc61d7c67e878e095668
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-04-02 13:10:54 -0500

    best method lof hybrid test

--- src/py/flwr_example/flower_test/server.py
+++ src/py/flwr_example/flower_test/server.py
@@ -533,8 +533,8 @@ class AggregateCustomMetricStrategy(fl.server.strategy.FedAvgM):
             for proxy, client in new_results:
                 newClientIDs.append(client.metrics["clientID"])
 
-            #lr_vector = compute_robustLR(new_update_dict, len(new_update_dict.keys())*.25)
-            lr_vector = compute_robustLR(new_update_dict, 3)
+            lr_vector = compute_robustLR(new_update_dict, len(new_update_dict.keys())*.25)
+            #lr_vector = compute_robustLR(new_update_dict, 3)
             print("LR vector based on hybrid method:")
             print(lr_vector)
 
@@ -884,7 +884,7 @@ def main():
     else:
         model = utils.CNN_MNIST()
         ct = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        filename = "hybrid/offset0.1/adjustLOFDiscard/hybrid_poison_50_discard_30_threshold_2_test_fedemnist_33_clients_500rounds_" + str(ct) + ".txt"
+        filename = "bestMethod/offset0.1/lofHybrid_poison_50_old_threshold_method_test_fedemnist_33_clients_" + str(ct) + ".txt"
         with open(filename, "w") as f:
             print("Running fedemnist test", file=f)
 
