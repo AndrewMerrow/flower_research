11532964b30bd169ea7238624d31b33d22ad8d11
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2024-05-01 08:18:14 -0500

    fmnist params

--- src/py/flwr_example/flower_test/server.py
+++ src/py/flwr_example/flower_test/server.py
@@ -905,7 +905,7 @@ def main():
     model_parameters = [val.cpu().numpy() for _, val in model.state_dict().items()]
 
     # Create strategy
-    num_agents = 33 if selectedDataset == "fedemnist" else 40
+    num_agents = 33 if selectedDataset == "fedemnist" else 10
     #strategy = fl.server.strategy.FedAvg(
     strategy = AggregateCustomMetricStrategy(
         min_fit_clients=num_agents,
@@ -923,7 +923,7 @@ def main():
     # Start Flower server for four rounds of federated learning
     fl.server.start_server(
         server_address="10.100.116.10:8080",
-        config=fl.server.ServerConfig(num_rounds=1 if selectedDataset == "fedemnist" else 1),
+        config=fl.server.ServerConfig(num_rounds=100 if selectedDataset == "fedemnist" else 100),
         strategy=strategy,
     )
 
